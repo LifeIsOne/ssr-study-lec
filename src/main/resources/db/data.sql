8f46a671e48c69b1c54530b42ae9927098da4348
LifeIsOne <didtmdgh258@gmail.com>
2024-04-07 16:57:11 +0900

    reply테이블, 더미 추가

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -6,3 +6,8 @@ insert into board_tb(title, content, user_id, created_at) values('제목1','내�
 insert into board_tb(title, content, user_id, created_at) values('제목2','내용2',1,now());
 insert into board_tb(title, content, user_id, created_at) values('제목3','내용3',2,now());
 insert into board_tb(title, content, user_id, created_at) values('제목4','내용4',3,now());
+
+INSERT INTO reply_tb(comment, created_at) VALUES('댓글 1', now());
+INSERT INTO reply_tb(comment, created_at) VALUES('댓글 2', now());
+INSERT INTO reply_tb(comment, created_at) VALUES('댓글 3', now());
+INSERT INTO reply_tb(comment, created_at) VALUES('댓글 4', now());
